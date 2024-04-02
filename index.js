9aa83824410c9a1b6d0bfdb5d5a4d262b9148eb5
+ <+>
2024-04-02 11:20:06 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function reducer(batches) {
+    let totalBatteries = 0;
+    for (const batch of batches) {
+        totalBatteries += batch;
+      }
+      return totalBatteries;
+}
+
+const totalBatteries = reducer(batteryBatches);
+console.log(totalBatteries);
